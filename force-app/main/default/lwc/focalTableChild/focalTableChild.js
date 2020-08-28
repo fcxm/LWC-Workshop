dadd011a342c5c0a8cc7817c3a031c1fbe237d33
charan <charan.kundeti@focalcxm.com>
2020-08-28 18:05:42 +0530

    Charan Lwc Initial Commit

---
+++ force-app/main/default/lwc/focalTableChild/focalTableChild.js
@@ -0,0 +1,30 @@
+import { LightningElement,track,api } from 'lwc';
+
+export default class FocalRowComponent extends LightningElement {
+    @api fieldValue;
+    @api recordId;
+    @api isEdit;
+    connectedCallback(){
+      
+    }
+    editRecord(event){
+        console.log("-->"+this.recordId);
+        var rowId = this.recordId;
+        const rowDataEvent = new CustomEvent("editaccount", {
+            detail: { rowId }
+          });
+          // Fire the custom event
+          this.dispatchEvent(rowDataEvent);
+       // console.log("-->"+JSON.stringify(this.fieldValue));
+
+    }
+
+    handleClick(event) {
+        console.log("In HandleClick");
+        const recId = event.target.name;
+        this.rec2Id = event.currentTarget.name;
+        console.log("Selected Account Id-->", recId);
+        console.log("Selected Account Id rec2Id -->", this.rec2Id);
+      //  this.open = true;
+    }
+}
